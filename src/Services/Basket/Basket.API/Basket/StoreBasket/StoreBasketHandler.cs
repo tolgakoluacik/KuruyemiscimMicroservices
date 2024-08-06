2487ebc6a8d27f3d4502347a5126fbfb3943e6de
Tolga <tolgakoluacik@outlook.com>
2024-08-06 22:58:42 +0300

    - Developed Basket Operations - Injected Custom Exception Handler - Created DB as a docker container

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Card can not be null!");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Username is required!");
+        }
+    }
+
+    public class StoreBasketCommandHandler
+        (IBasketRepository repository)
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            //TODO update cache
+
+            await repository.StoreBasket(command.Cart, cancellationToken);
+
+            return new StoreBasketResult(command.Cart.UserName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Marten;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Basket.API.Data;
+global using FluentValidation;
+global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
